e371084a72436d1cdcf7ae3ee8d532a6e44a9724
WaiChungWong <johnwongwwc@gmail.com>
2020-12-28 15:40:03 +0000

    Deployment at 2020-12-28

---
+++ build/precache-manifest.d745b5e6171554205e221cbb72863bda.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16d0e6f3b8cd49b058cec7e669e37f15",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce5b37a091538270cf4f",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b2531dcac628074e67c4",
+    "url": "/static/js/2.562ec2a9.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.562ec2a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce5b37a091538270cf4f",
+    "url": "/static/js/main.b3674492.chunk.js"
+  },
+  {
+    "revision": "6521ec5b530f84fbddc2",
+    "url": "/static/js/runtime-main.26317727.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
